789a3eddec54bcd9be90ef210f04e2a9ea3f0f85
behrenreich <51254514+behrenreich@users.noreply.github.com>
2019-08-10 01:11:11 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,11 @@
 var Client = require("mpp-client");
 var client = new Client("ws://www.multiplayerpiano.com:443");
-client.setChannel("loobby");
-
-var chat;
+client.setChannel("NMPB Lobby");
 
 (function checkChat(){
 	client.start();
 	client.once("c", msg => {
-		if (!chat) console.log(chat = msg.c);
-		else msg.c.forEach(thisChat => {
-			let lastChat = chat[chat.length - 1];
-			if (thisChat.t > lastChat.t) {
-				chat.push(thisChat);
-				console.log(thisChat);
-			};
-		});
 		client.stop();
-		setTimeout(checkChat, 10000);
+		setTimeout(checkChat, 1000);
 	});
-})();  
+})(); 
